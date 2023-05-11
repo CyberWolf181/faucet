5d065e726bb0b7f733c4fe9b4c97172279779201
Ekrem Kurt <e.kurt@shopware.com>
2023-05-11 20:09:24 +0200

    faucet app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import {ethers} from "ethers";
+import {useState, useEffect} from "react";
+import FaucetAbi from "./abis/Faucet.json";
+import Swal from 'sweetalert2';
+
+
+const faucetContractAddress = "0x891B7464cDc0f11f6a93b4C390fD01Cc4155aEFf";
 
 function App() {
+
+  const[walletAddress, setWalletAddress] = useState("");
+  const[provider, setProvider] = useState("");
+
+  useEffect(() =>{
+    connectWallet();
+  }, [walletAddress])
+
+  const connectWallet = async () => {
+    if(typeof window.ethereum != "undefined"){
+      try{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        const accounts = await provider.send("eth_requestAccounts");
+
+        setProvider(provider);
+
+        setWalletAddress(accounts[0]);
+        console.log(accounts);
+      } catch(err){
+        console.log(err)
+      }
+    }
+  }
+
+  const getTokens = async () => {
+    try {
+      const contract = new ethers.Contract(
+        faucetContractAddress,
+        FaucetAbi,
+        provider.getSigner()
+      );
+
+      const transaction = await contract.requestToken();
+
+      if (transaction.hash) {
+        Swal.fire({
+          title: "Success!",
+          html: `Check Your <b>Wallet</b>,
+          '<a href="https://sepolia.etherscan.io/tx/${transaction.hash}" target="_blank" rel="noopener noreferrer">ETH TX Hash</a> '
+          'at Etherscan`,
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+      }
+      console.log("transaction", transaction);
+    } catch (error) {
+      const regex = /reason="([^"]+)"/;
+
+      Swal.fire({
+        title: error.message.match(regex) == null ? "Transaction Failed" : error.message.match(regex)[0],
+        text: "Do you want to continue",
+        icon: "error",
+        confirmButtonText: "Close",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size4">MyToken (MTK) Faucet</h1>
+          </div>
+          <div id="navbar-menu"className="navbar-menu">
+            <div className="navbar-end">
+              <button className="button is-white connect-wallet" onClick={connectWallet}>
+
+                {walletAddress ? `Connected: ${walletAddress.substring(0,6)}...${walletAddress.substring(36)}` : "Connect Wallet"}
+              
+              </button>
+            </div>
+            
+          </div>
+        </div>
+      </nav>
+      <section className='hero'>
+        <div className='faucet-hero-body'>
+          <div className='box'>
+            <input 
+                type="text" 
+                className="" 
+                placeholder='Enter your wallet address (0x....)'
+                defaultValue={walletAddress}
+                />
+            <button className='button' onClick={getTokens}>Get tokens</button>
+          </div>
+
+        </div>
+
+      </section>
+    </>
   );
 }
 
